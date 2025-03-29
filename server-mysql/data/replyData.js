e2cde51f7c88093e9f651bbacbcfd12159c3ec24
Mandakh Tsogt <mandah0628@gmail.com>
2025-03-29 12:47:51 -0600

    added db operations

--- server-mysql/data/replyData.js
+++ server-mysql/data/replyData.js
@@ -6,21 +6,30 @@ const mysqlPool = require('../config/mysql');
  * @returns The INSERT operation metadata object from a promise that reolves into an an array.
  */
 const createReplyDb = async (replyData) => {
-   const {id, postId, userId, parentReplyId, comment, imageUrl} = replyData;
-   const values = [id, postId, userId, parentReplyId || null, comment, imageUrl || null];
+   const {id, postId, userId, parentReplyId, comment, imageId} = replyData;
+   const values = [id, postId, userId, parentReplyId || null, comment, imageId || null];
 
-   const query = `INSERT INTO replies (id, postId, userId, parentReplyId, comment, imageUrl) VALUES(?,?,?,?,?,?)`
+   const query = `INSERT INTO replies (id, postId, userId, parentReplyId, comment, imageId) VALUES(?,?,?,?,?,?)`
 
    const [result] = await mysqlPool.execute(query, values);
    return result;
 }
 
 
-const createImageDb = async (params) => {
-    
+/**
+ * Fetches all replies from the replies table that's associated with the post id
+ * @param {number} postId The post id.
+ * @returns A promise that resolves into an array of replies if records are found.
+ * Empty array if no records are found.
+ */
+const getAllPostRepliesDb = async (postId) => {
+    const query = `SELETC * FROM replies WHERE postId = ? ORDER BY createdAt DESC`;
+    const [replies] = await mysqlPool.execute(query, [postId] );
+
+    return replies;
 }
 
 module.exports = {
     createReplyDb,
-
+    getAllPostRepliesDb
 }
