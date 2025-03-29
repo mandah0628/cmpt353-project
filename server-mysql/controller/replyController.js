d5c866c7f769a3a497bba3b84c285ce63b39a731
Mandakh Tsogt <mandah0628@gmail.com>
2025-03-29 12:47:41 -0600

    implemented controller

--- server-mysql/controller/replyController.js
+++ server-mysql/controller/replyController.js
@@ -1,12 +1,26 @@
 const {
     createReplyDb
 } = require('../data/replyData')
+const uploadImage = require('../utils/uploadImage')
 
 
 const createReply = async (req,res) => {
     try {
+        // extract data
         const userId = req.user.id;
+        const image = req.file;
 
+        // if there is an image file, upload to couchdb and get the id
+        // if not, set it tonull
+        const imageId = image ? await uploadImage(image) : null;
+
+        // prepare reply data
+        const replyData = {...req.body , imageId, userId};
+
+        // create reply record in the replies table
+        await createReplyDb(replyData);
+    
+        res.status(200);
         
     } catch (error) {
         console.error("Error creating reply:", error);
@@ -16,5 +30,5 @@ const createReply = async (req,res) => {
 
 
 module.exports = {
-
+    createReply,
 }
