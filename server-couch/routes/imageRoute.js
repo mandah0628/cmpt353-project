42f95def4ecc88911ee5ddf600d3a23f1c871bdb
Matt Tsogt <mandah0628@gmail.com>
2025-03-30 21:56:43 -0600

    defined routes

--- server-couch/routes/imageRoute.js
+++ server-couch/routes/imageRoute.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-//controller imports
+// middleware imports
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({storage})
+// controller imports
+const { uploadImage, getAllReplyImages } = require('../controller/imageController')
+
+// upload image to couchdb route
+router.post("/upload-image", upload.single("image"), uploadImage);
+
+// fetch all reply images belonging to a post
+router.get("/get-images", getAllReplyImages);
 
 
 module.exports = router;
