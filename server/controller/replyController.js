85e3fd7d16543dda3cbf06cc7c5da4c8f262dd25
Matt Tsogt <mandah0628@gmail.com>
2025-03-31 18:55:59 -0600

    added controller for fetching all replies

--- server/controller/replyController.js
+++ server/controller/replyController.js
@@ -1,26 +1,26 @@
 const {
-    createReplyDb
+    createReplyDb,
+    getAllPostRepliesDb
 } = require('../data/replyData')
-const uploadImage = require('../utils/uploadImage')
 
 
+// creates a reply record in the 'replies' table
 const createReply = async (req,res) => {
     try {
         // extract data
         const userId = req.user.id;
         const image = req.file;
 
-        // if there is an image file, upload to couchdb and get the id
-        // if not, set it tonull
-        const imageBuffer = image ? await uploadImage(image) : null;
+        // if there is an image file, get the binary data
+        const imageBuffer = image ? image.buffer : null;
 
         // prepare reply data
-        const replyData = {...req.body , imageId, userId};
+        const replyData = {...req.body , image : imageBuffer, userId};
 
         // create reply record in the replies table
         await createReplyDb(replyData);
     
-        res.status(200);
+        res.status(200).send("OK");
         
     } catch (error) {
         console.error("Error creating reply:", error);
@@ -29,6 +29,36 @@ const createReply = async (req,res) => {
 }
 
 
+// gets all reply records from the 'replies' table associated with the post
+const getAllReplies = async (req,res) => {
+    try {
+        // extract data
+        const {postId} = req.params;
+
+        // fetch replies
+        const replies = await getAllPostRepliesDb(postId);
+
+        const formattedReplies = replies.map((reply => {
+            if(reply.image) {
+                const base64Image = Buffer.from(reply.image).toString("base64");
+
+                return { ...reply, image: `data:${reply.imageMimeType};base64,${base64Image}` };
+            } else {
+                return reply;
+            }
+        }));
+
+        res.status(200).json({formattedReplies});
+
+    } catch (error) {
+        console.error("Error fetching replies:", error);
+        res.status(500).json({message: "Server error"});
+    }
+    
+}
+
+
 module.exports = {
     createReply,
+    getAllReplies
 }
